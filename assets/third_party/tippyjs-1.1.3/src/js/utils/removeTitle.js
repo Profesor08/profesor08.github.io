de11eb4792fc06073a52b96f6a954c78bd0abe09
Profesor08 <online7890@gmail.com>
2018-03-04 10:44:13 +0200

    clear repository from old files

--- assets/third_party/tippyjs-1.1.3/src/js/utils/removeTitle.js
+++
@@ -1,9 +0,0 @@
-/**
-* Removes the title from the tooltipped element
-* @param {Element} el
-*/
-export default function removeTitle(el) {
-    const title = el.getAttribute('title')
-    el.setAttribute('data-original-title', title || 'html')
-    el.removeAttribute('title')
-}
